34d30eb9da9bb281590bbcfe99f8a6a720bfc72f
szaumoor <szaumoor@tuta.io>
2026-07-14 16:20:49 +0200

    WoB and Energy Drain fixes
    Signed-off-by: szaumoor <szaumoor@tuta.io>

--- ZSTweaks/dlg/zs_wish_wail_banshee.d
+++ ZSTweaks/dlg/zs_wish_wail_banshee.d
@@ -2,4 +2,4 @@ REPLACE_TRANS_ACTION ~WISH~
 BEGIN 4 END
 BEGIN 0 END
 ~ReallyForceSpell(Myself,WIZARD_WAIL_OF_THE_BANSHEE)~
-~ReallyForceSpellRES(zstwswl, Myself)~
+~ReallyForceSpellRES("zstwswl", Myself)~
